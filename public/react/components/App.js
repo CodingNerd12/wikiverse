3b132d8ba864c1b2cc70ff2616514269e8cc113d
Kris A <codewriting12@gmail.com>
2022-09-14 15:48:57 -0400

    adding button for creating page and base JS files

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { PagesList } from './PagesList';
+import { Article } from './Article';
+import { Form } from './Form';
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
@@ -7,6 +9,13 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [pages, setPages] = useState([]);
+	const [isClicked, setIsClicked] = useState(false);
+
+	//set the state for getting article data (articleData, setArticleData)
+	const [articleData, setArticleData] = useState();
+
+	//set a new state boolean for when an article is added
+	const [isAddingArticle, setIsAddingArticle] = useState(false);
 
 	async function fetchPages(){
 		try {
@@ -24,9 +33,18 @@ export const App = () => {
 
 	return (
 		<main>	
-      <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
+			{isClicked} ? ( <Article articleData = {articleData} setIsClicked = {setIsClicked}/>) :
+			isAddingArticle ? (<Form setIsAddingArticle	= {setIsAddingArticle}/>) : (
+				<div>
+					<h1>WikiVerse</h1>
+					<h2>An interesting 📚</h2>
+					<PagesList pages={pages} />
+
+					{/* add button for creating the page*/}
+					<button onClick{() => setIsAddingArticle(true)}>Create Page</button>
+				</div>
+			)
+      
 		</main>
 	)
 }
